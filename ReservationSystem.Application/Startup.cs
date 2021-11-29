76758ca5aded0e57633ff7cbde2a25574cb2e546
Alexy <Alexy@DESKTOP-JI2TJNL>
2021-11-29 13:59:23 -0500

    Calling dependency injection from startup project

--- ReservationSystem.Application/Startup.cs
+++ ReservationSystem.Application/Startup.cs
@@ -1,13 +1,9 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using ReservationSystem.Root;
 
 namespace ReservationSystem.Application
 {
@@ -23,7 +19,11 @@ namespace ReservationSystem.Application
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+            CompositionRoot.InjectDependencies(services, Configuration);
+            services.AddControllersWithViews()
+                .AddNewtonsoftJson(options =>
+                options.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore
+                );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
